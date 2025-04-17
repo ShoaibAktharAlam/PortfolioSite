79039688bbef68bc68566f60d60853448ac77bd3
ShoaibAkhtarAlam <133253960+ShoaibAktharAlam@users.noreply.github.com>
2025-04-17 15:35:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ const themeToggle = document.querySelector('.theme-toggle');
 themeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 });
+
+const tabLinks = document.querySelectorAll(".tab-links");
+  const tabContents = document.querySelectorAll(".tab-contents");
+
+  tabLinks.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+      tabLinks.forEach(t => t.classList.remove("active-link"));
+      tabContents.forEach(c => c.classList.remove("active-tab"));
+
+      tab.classList.add("active-link");
+      tabContents[index].classList.add("active-tab");
+    });
+});
